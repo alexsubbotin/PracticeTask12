82e0ff1fb216690308527af8723a094290ed811b
Alexey Subbotin <subbotin910@gmail.com>
2018-05-03 13:14:57 +0500

    HeapSort added

--- PracticeTask12/PracticeTask12/Program.cs
+++ PracticeTask12/PracticeTask12/Program.cs
@@ -16,7 +16,7 @@ namespace PracticeTask12
         }
 
         // Bubble sort.
-        public static void BubbleSort(int[] arr, ref int SwapCount, ref int CompareCount)
+        public static void BubbleSort(ref int[] arr, ref int SwapCount, ref int CompareCount)
         {
             for(int i = 0; i < arr.Length; i++)
             {
@@ -24,6 +24,7 @@ namespace PracticeTask12
                 {
                     // Increasing the number of compares.
                     CompareCount++;
+
                     if(arr[j] > arr[j + 1])
                     {
                         int buf = arr[j];
@@ -35,6 +36,65 @@ namespace PracticeTask12
                     }
                 }
             }
+        }
+
+        public static void HeapSort(ref int[] arr, ref int SwapCount, ref int CompareCount)
+        {
+            // Step 1: creating a pyramid.
+            for(int i = arr.Length / 2 - 1; i >=0; i--)
+            {
+                int prev = i;
+                i = AddToPyramid(arr, i);
+                if (prev != i)
+                    ++i;
+            }
+
+            // Step 2: sorting.
+            for(int i = arr.Length - 1; i > 0; i--)
+            {
+                int buf = arr[i];
+                arr[i] = arr[0];
+                buf = arr[0];
+
+                int k = 0;
+                int prev = -1;
+
+                while(k != prev)
+                {
+                    prev = k;
+                    k = AddToPyramid(arr, k);
+                }
+            }
+        }
+
+        public static int AddToPyramid(int[] arr, int elemIndex)
+        {
+            int imax;
+
+            if (2 * elemIndex + 2 < arr.Length)
+            {
+                if (arr[2 * elemIndex + 1] < arr[2 * elemIndex + 2])
+                    imax = arr[2 * elemIndex + 2];
+                else
+                    imax = arr[2 * elemIndex + 1];
+            }
+            else
+                imax = 2 * elemIndex + 1;
+
+            if (imax >= arr.Length)
+                return elemIndex;
+
+            if(arr[elemIndex] < arr[imax])
+            {
+                int buf = arr[elemIndex];
+                arr[elemIndex] = arr[imax];
+                arr[imax] = buf;
+
+                if (imax < arr.Length / 2)
+                    elemIndex = imax;
+            }
+
+            return elemIndex;
         }
     }
 }
