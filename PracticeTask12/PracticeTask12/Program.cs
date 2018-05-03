d53ea7b9891040934cf278c65fbb52d6ceb79f9c
Alexey Subbotin <subbotin910@gmail.com>
2018-05-03 10:15:51 +0500

    initial commit

---
+++ PracticeTask12/PracticeTask12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeTask12
+{
+    class Program
+    {
+        // Task: compare two sorting algorithms – Bubble sort and Heapsort – on number of swaps and number of compares.
+        // Compare them using three types of arrays: 1. Sorted 2. Reverse sorted 3. Unsorted
+        // Student: Alexey Subbtoin. Group: SE-17-1.
+        static void Main(string[] args)
+        {
+        }
+    }
+}
